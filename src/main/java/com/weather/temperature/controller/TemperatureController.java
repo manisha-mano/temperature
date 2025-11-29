76462a87c18cb341b348c90f1153fc911b45de25
manisha-mano <manisha.mano1234@gmail.com>
2025-11-29 23:58:05 +0530

    Changes for @AllArgsConstructor

--- src/main/java/com/weather/temperature/controller/TemperatureController.java
+++ src/main/java/com/weather/temperature/controller/TemperatureController.java
@@ -1,6 +1,7 @@
 package com.weather.temperature.controller;
 
 import com.weather.temperature.service.TemperatureService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 @Controller
+@AllArgsConstructor
 public class TemperatureController {
 
     TemperatureService temperatureService;
-    TemperatureController(TemperatureService temperatureService) {
-        this.temperatureService = temperatureService;
-    }
+    //TemperatureController(TemperatureService temperatureService) {
+//        this.temperatureService = temperatureService;
+//    }
+
+
 
 
     @GetMapping("/v1/weather/getTemperature")
